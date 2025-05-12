af8c5cc20d2872f93eeeac32fa72d44a8f410357
INFOR\matteou <m.urso@esaedro.com>
2025-05-12 13:03:18 +0200

    Add initial page and font configurations; remove default home page

---
+++ app/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter, JetBrains_Mono, Sora } from "next/font/google";
+
+export const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const jetbrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrains",
+  subsets: ["latin"],
+});
+
+export const sora = Sora({
+  variable: "--font-sora",
+  subsets: ["latin"],
+});
