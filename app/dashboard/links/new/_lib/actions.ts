c22972fa3b348da059bca7201da6fcf81a928f06
INFOR\matteou <m.urso@esaedro.com>
2025-05-14 17:48:47 +0200

    feat: refactor createLink function to improve error handling for existing short codes; streamline database insertion logic

--- app/dashboard/links/new/_lib/actions.ts
+++ app/dashboard/links/new/_lib/actions.ts
@@ -1,13 +1,12 @@
 "use server";
 
-// import { revalidatePath } from 'next/cache';
-// import { redirect } from "next/navigation";
 import { State } from "../_types/state";
 import { FormSchema } from "./form-schema";
 import { db } from "@/db/drizzle";
 import { link } from "@/db/schema";
 import { getAuthSession } from "@/lib//auth-session";
 import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 import { ZodError } from "zod";
 
 export async function createLink(_: State, formData: FormData): Promise<State> {
@@ -31,6 +30,36 @@ export async function createLink(_: State, formData: FormData): Promise<State> {
       where: eq(link.short_code, shortCode),
     });
     if (existingLink) {
+      throw new Error("Conflict");
+    }
+
+    await db.insert(link).values({
+      userId: userId,
+      original_url: safeData.original_url,
+      short_code: shortCode,
+    });
+
+    revalidatePath("/dashboard/links");
+
+    return {
+      status: "success",
+      message: "Link created successfully!",
+    };
+  } catch (error) {
+    console.error("Error creating link:", error);
+
+    if (error instanceof ZodError) {
+      return {
+        status: "error",
+        message: "Validation error",
+        errors: error.errors.map((err) => ({
+          field: err.path.join("."),
+          message: err.message,
+        })),
+      };
+    }
+
+    if (error instanceof Error && error.message === "Conflict") {
       return {
         status: "error",
         message:
@@ -45,39 +74,9 @@ export async function createLink(_: State, formData: FormData): Promise<State> {
       };
     }
 
-    const insertLink = await db
-      .insert(link)
-      .values({
-        userId: userId,
-        original_url: safeData.original_url,
-        short_code: shortCode,
-      })
-      .returning();
-    console.log("Insert link result:", insertLink);
-
-    return {
-      status: "success",
-      message: "Link created successfully!",
-    };
-  } catch (error) {
-    if (error instanceof ZodError) {
-      return {
-        status: "error",
-        message: "Validation error",
-        errors: error.errors.map((err) => ({
-          field: err.path.join("."),
-          message: err.message,
-        })),
-      };
-    }
-
     return {
       status: "error",
       message: "An error occurred while creating the link. Please try again.",
     };
   }
-
-  // Revalidate the cache for the links page and redirect the user.
-  // revalidatePath("/dashboard/links");
-  // redirect("/dashboard/links");
 }
